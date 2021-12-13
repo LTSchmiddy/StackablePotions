31e25852054eaf03496b5496380dd3f998e2dbea
LT_Schmiddy (Alex Schmid) <schmidalex1996@gmail.com>
2021-12-12 19:35:55 -0500

    updated

--- src/main/java/cursedflames/stackablepotions/mixin/BrewingStandScreenHandlerMixin.java
+++ src/main/java/cursedflames/stackablepotions/mixin/BrewingStandScreenHandlerMixin.java
@@ -1,8 +1,6 @@
 package cursedflames.stackablepotions.mixin;
 
-import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
